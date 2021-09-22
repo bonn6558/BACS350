7010c30cd6f7ad1448df1f4fa36803e0e8a5e3e8
Caden-Bonnell <70333456+Caden-Bonnell@users.noreply.github.com>
2021-09-22 08:36:30 -0600

    added week 4

--- week4/Superhero/config/urls.py
+++ week4/Superhero/config/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from hero.views import IndexView
+from hero.views import IndexView, HeroListView, HeroDetailView
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view()),
+    path('hero/', HeroListView.as_view()),
+    path('hero/<int:pk>', HeroDetailView.as_view())
 ]
